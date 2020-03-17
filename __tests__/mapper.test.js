29f12af30c7c4247ae4d5651c19cb4003ff11073
Yurii Mykhailiak <ghost86421@gmail.com>
2020-03-17 20:43:48 +0200

    Adjust mapper for nester type

--- __tests__/mapper.test.js
+++ __tests__/mapper.test.js
@@ -1,6 +1,6 @@
 import { createHighLevelMap } from '../utils/mapper';
 
-it('should create map by context level', () => {
+it('should create map by context level (shallow type)', () => {
   const input = {
     'order-support-system': {
       name: 'Name',
@@ -21,6 +21,63 @@ it('should create map by context level', () => {
   };
   const result = { 'order-support-system': ['order-processing-system'] };
 
+  expect(createHighLevelMap(input)).toEqual(result);
+});
+
+it('should create map by context level (nested type)', () => {
+  const input = {
+    'order-support-system': {
+      name: 'Name',
+      container: {
+        'order-support-system-foo': {
+          name: 'Foo',
+          relations: {
+            to: {
+              'back-end': 'posts Orders',
+            },
+          },
+        },
+      },
+    },
+    'order-processing-system': {
+      name: 'Order processing',
+      container: {
+        'back-end': {
+          name: 'Application Back-end part',
+        },
+      },
+    },
+  };
+  const inputSecondOption = {
+    'order-support-system': {
+      name: 'Name',
+      container: {
+        'order-support-system-foo': {
+          name: 'Foo',
+          component: {
+            'order-suppoert-system-foo2': {
+              name: 'Foo2',
+              relations: {
+                to: {
+                  'back-end': 'posts Orders',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    'order-processing-system': {
+      name: 'Order processing',
+      container: {
+        'back-end': {
+          name: 'Application Back-end part',
+        },
+      },
+    },
+  };
+  const result = { 'order-support-system': ['order-processing-system'] };
 
   expect(createHighLevelMap(input)).toEqual(result);
+  expect(createHighLevelMap(inputSecondOption)).toEqual(result);
 });
--- utils/mapper.js
+++ utils/mapper.js
@@ -22,12 +22,26 @@ const findParentEntity = (entity, lookingKey, parent) => {
 const findParentEntities = (entries, keys = []) =>
   keys.map((k) => findParentEntity(entries, k));
 
-export const createHighLevelMap = (data = {}) => {
+export const createHighLevelMap = (data = {}, key, globalContext) => {
   const entries = Object.entries(data);
 
   return entries.reduce((acc, [id, entity]) => {
+    const computedKey = key || id;
+    const context = globalContext || data;
+
     if (entity.relations && entity.relations.to) {
-      acc[id] = findParentEntities(data, Object.keys(entity.relations.to));
+      acc[computedKey] = findParentEntities(context, Object.keys(entity.relations.to));
+
+      return acc;
+    }
+
+    const levelKey = innerLevels.find(l => entity.hasOwnProperty(l));
+
+    if (levelKey) {
+      return {
+        ...acc,
+        ...createHighLevelMap(entity[levelKey], computedKey, context),
+      }
     }
 
     return acc;
